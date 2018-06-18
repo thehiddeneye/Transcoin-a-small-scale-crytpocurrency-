0f2157a659076982a0df5c2a3aae8b0a0a4119da
thehiddeneye <abhishek2github@gmail.com>
2018-06-18 16:07:12 +0530

    chains of other nodes replcated

--- server-code.py
+++ server-code.py
@@ -35,8 +35,6 @@ peer_nodes = []
 
 mining = True
 
-@staticmethod
-
 @node.route('/txion',methods = ['POST'])
 def transaction():
     new_txion =request.get_json()
@@ -48,4 +46,39 @@ def transaction():
     print "AMOUNT:{}".format(new_txion['amount'])
 
     return "Transaction submitted successfully \n" 
+
+@node.route('/blocks',methods = ['GET'])
+
+def get_blocks():
+    chain_to_send =blockchain
+
+    for i in range(len(chain_to_send)):
+        block =chain_to_send[i]
+        block_index = str(block.index)
+        block_timestamp =str(block.timestamp)
+        block_data = str(block.data)
+        block_hash = str(block.hash)
+
+        chain_to_send[i] = {
+            "index":block_index,
+            "timestamp":block_timestamp,
+            "data":block_data,
+            "hash":block_hash
+
+        }
+
+    chain_to_send =json.dumps(chain_to_send)
+    return chain_to_send
+
+def find_new_chains():
+    other_chains = []
+
+    for node_url in peer_nodes:
+        block =requests.get(node_url + "/blocks").content
+        block = json.loads(block)
+        other_chains.append(block)
+    return other_chains
+
     
+        
+   
