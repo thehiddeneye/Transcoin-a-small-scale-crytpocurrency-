7a7e52b4a736b1311cfba1569f211d57b7d21d23
thehiddeneye <abhishek2github@gmail.com>
2018-06-18 14:34:40 +0530

    transaction submission added

--- server-code.py
+++ server-code.py
@@ -18,11 +18,34 @@ class Block:
         sha = hasher.sha256()
         sha.update(str(self.index)+str(self.timestamp)+str(self.data)+str(self.previous_hash))
         return sha.hexdigest()
+ 
+def create_genesis_block():
+    return Block(0,date.datetime.now(),{
+        "proof-of-work": 9,
+        "transactions":None
+    },"0")
 
-    @staticmethod 
-    def create_genesis_block():
-       return Block(0,date.datetime.now(),{
-           "proof-of-work": 9,
-           "transactions":None
-       },"0")
+miner_address = "q3nfwerfd-random-miner-address-wefgcvgfvgf"
 
+blockchain = []
+blockchain.append(create_genesis_block())
+
+this_nodes_transactions = []
+peer_nodes = []
+
+mining = True
+
+@staticmethod
+
+@node.route('/txion',methods = ['POST'])
+def transaction():
+    new_txion =request.get_json()
+    this_nodes_transactions.append(new_txion)
+
+    print "New transaction "
+    print "From:{}".format(new_txion['from'].encode('ascii','replace')) 
+    print "To:{}".format(new_txion['to'].encode('ascii','replace'))
+    print "AMOUNT:{}".format(new_txion['amount'])
+
+    return "Transaction submitted successfully \n" 
+    
