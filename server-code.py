978e17f354edf7451e115fdc7488b4cdd291f30f
thehiddeneye <abhishek2github@gmail.com>
2018-06-18 12:32:43 +0530

    encryption added

--- server-code.py
+++ server-code.py
@@ -13,3 +13,9 @@ class Block:
         self.data = data
         self.previous_hash = previous_hash
         self.hash =self.hash_block()
+
+    def hash_block(self):
+        sha = hasher.sha256()
+        sha.update(str(self.index)+str(self.timestamp)+str(self.data)+str(self.previous_hash))
+        return sha.hexdigest()
+        
