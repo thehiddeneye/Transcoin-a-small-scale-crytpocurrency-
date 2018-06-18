cfb4388dd504f1185ccea8d4f076f89d2c9fbf2c
thehiddeneye <abhishek2github@gmail.com>
2018-06-18 16:38:03 +0530

    A method for mining added

--- server-code.py
+++ server-code.py
@@ -95,8 +95,38 @@ def proof_of_work(last_proof):
         incrementor +=1
     return incrementor
 
+@node.route('/mine',methods = ['GET'])
+def mine():
+    last_block =blockchain[len(blockchain)-1]
+    last_proof = last_block.data['proof_of_work']
+    
+    proof = proof_of_work(last_proof)
 
+    this_nodes_transactions.append({
+        "from":"network",
+        "to":miner_address,
+        "amount":1
+    })
+    
+    new_block_data = {
+        "proof-of-work": proof,
+        "transactions":list(this_nodes_transactions)
+    }
 
+    new_block_index = last_block.index+1 
+    new_block_timestamp = date.datetime.now()
+    last_block_hash = last_block.hash
 
-        
-   
+    this_nodes_transactions[:] = []
+    mined_block =Block(new_block_index,new_block_timestamp,new_block_data ,last_block_hash)
+    blockchain.append(mined_block)
+    
+
+    return json.dumps({
+        "index":new_block_index,
+        "timestamp":new_block_timestamp,
+        "data":new_block_data,
+        "hash":last_block_hash
+    }) +"\n"
+
+node.run()
